df99ba5c20eb924d492a393492b36967c9939de9
Yahya Salimi <82891624+moyamoha@users.noreply.github.com>
2022-02-26 19:03:51 +0200

    Update views.py

--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,9 @@
 import email
 from rest_framework.views import APIView
 from rest_framework import generics
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.decorators import authentication_classes
-from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
